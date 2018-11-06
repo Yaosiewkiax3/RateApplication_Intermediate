6f5f8d53a3eb7380af49924305b59c23492bd675
170313Q@mymail.nyp.edu.sg <170313Q@mymail.nyp.edu.sg>
2018-11-06 18:33:58 +0800

    finished screen 1

--- app/src/main/java/com/example/user/movierateapplication/AddMovie.kt
+++ app/src/main/java/com/example/user/movierateapplication/AddMovie.kt
@@ -2,8 +2,9 @@ package com.example.user.movierateapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
+import android.view.View
+import android.widget.*
+import kotlinx.android.synthetic.main.activity_add_movie.*
 
 class AddMovie : AppCompatActivity() {
 
@@ -12,13 +13,61 @@ class AddMovie : AppCompatActivity() {
         setContentView(R.layout.activity_add_movie)
 
 
+
+        cbAudience.setOnCheckedChangeListener{checkbox,isChecked->
+            if(isChecked){
+                cbReasons.visibility = View.VISIBLE
+            }
+            else{
+                cbReasons.visibility = View.INVISIBLE
+            }
+        }
+
         val button = findViewById<Button>(R.id.button2)
         button.setOnClickListener{
-            val text = "Hello world"
+            val ptMovieName = findViewById<TextView>(R.id.ptMovieName)
+            val movieName :String? = ptMovieName.text.toString()
+            val ptDesc = findViewById<TextView>(R.id.ptDesc)
+            val desc :String? = ptDesc.text.toString()
+            val ptRelease = findViewById<TextView>(R.id.ptReleaseDate)
+            val releaseDate :String? = ptRelease.text.toString()
+            val radioLangGroup = findViewById<RadioGroup>(R.id.rgLang)
+            val idSelected = radioLangGroup.checkedRadioButtonId
+            val radioLangText = findViewById<RadioButton>(idSelected).text
+
+            if(movieName.isNullOrBlank()){
+                ptMovieName.setError("Field empty")
+            }
+            if(desc.isNullOrBlank()){
+                ptDesc.setError("Field empty")
+            }
+            if(releaseDate.isNullOrBlank()){
+                ptRelease.setError("Field empty")
+            }
             val duration = Toast.LENGTH_SHORT
 
-            val toast = Toast.makeText(applicationContext, text, duration)
+            val suitable4Aud = cbAudience.isChecked.toString()
+            var toastText = "Title = $movieName \n Overview = $desc \n Release date = $releaseDate \n Language = $radioLangText \n Not suitable for all ages = $suitable4Aud"
+            if(cbAudience.isChecked){
+                toastText += "\n Reasons:"
+                if(cbViolence.isChecked){
+                    toastText += "\n Violence"
+                }
+                if(cbLanguage.isChecked){
+                    toastText += "\n Language"
+                }
+            }
+            val toast = Toast.makeText(applicationContext, toastText, duration)
             toast.show()
+            }
+
+
+
+
         }
+
+
+
     }
-}
+
+

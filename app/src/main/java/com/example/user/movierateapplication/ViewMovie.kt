a0d3293b42efb309d39178ab13aeec680031763b
170313Q@mymail.nyp.edu.sg <170313Q@mymail.nyp.edu.sg>
2018-11-10 17:58:15 +0800

    finished screen 3?

---
+++ app/src/main/java/com/example/user/movierateapplication/ViewMovie.kt
@@ -0,0 +1,18 @@
+package com.example.user.movierateapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewMovie : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_movie)
+
+
+        }
+
+
+    }
+
+

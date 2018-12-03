e8a207585012dc606ce2d1f5522382c09bb7683d
Yaosiewkiax3 <170313Q@mymail.nyp.edu.sg>
2018-12-03 17:22:49 +0800

    forgot to check

---
+++ app/src/main/java/com/example/user/movierateapplication/RateMovie.kt
@@ -0,0 +1,18 @@
+package com.example.user.movierateapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RateMovie : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rate_movie)
+
+
+        }
+
+
+    }
+
+
